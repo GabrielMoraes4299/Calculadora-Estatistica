c7843a95e1ff047c003e231c06d046b7886b64bb
= <=>
2026-05-07 11:20:20 -0300

    Criando função para mediana.

--- main.py
+++ main.py
@@ -23,6 +23,38 @@ def desvioPadrao(lst_dados):
 def coeficienteVar(dp):
 	return (dp / media) * 100
 
+def mediana(lst_dados):
+	mediana = 0
+	fi_list = []
+	for i in lst_dados:
+		fi_list.extend([i["xi"]] * i["fi"])
+	
+	if len(fi_list) % 2 == 0:
+		meio = len(fi_list) // 2
+
+		dados_meio = fi_list[meio - 1: meio + 1]
+
+		mediana = (dados_meio[0] + dados_meio[1]) / 2
+	else:
+		meio = fi_list[len(fi_list) // 2]
+		mediana = meio
+	
+	return mediana
+
+
+
+# def moda(lst_dados, media):
+# 	fi_anterior = 0
+
+# 	for i in lst_dados:
+# 		delta1 = i["fi"] - fi_anterior
+# 		i["fi"] + (delta1/ delta1)
+# 		fi_anterior = i["fi"]
+
+
+
+	
+
 while True:
 	xi = float(input("Digite o xi: "))
 	fi = int(input("Digite o fi: "))
@@ -44,3 +76,5 @@ dp = desvioPadrao(lst_dados)
 cv = coeficienteVar(dp)
 
 print(f"Desvio Padrão: {dp}\nCV: {cv}%")
+
+print(f"A médiana é: {mediana(lst_dados)}")
